81597f772f341a60e7a9e4b7d0df480401cd2706
Nick <nmapsy@gmail.com>
2014-02-14 20:20:35 -0500

    getseq.sh: Extract a sequence from a FASTA file given coordinates

---
+++ getseq.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -ue
+
+if [[ $# -ge 1 ]]; then
+  region=$1
+else
+  command=$(basename $0)
+  echo "USAGE: $command region seq.fa [seq2.fa [seq3.fa [...]]]
+       cat seq.fa | $command region
+  e.g. $command chrM:1020-1080 hg19.fa
+Use bioawk to print the sequence at the given coordinates in a FASTA (or FASTQ)
+file. Use UCSC format for \"region\". Chromosome and ending coordinate are
+optional. If no ending coordinate, it prints a single base at the start
+coordinate. If no chromosome, it prints for every chromosome in the file.
+WARNING:
+bioawk seems to load each chromosome entirely into memory. For hg19.fa, it uses
+about 1GB." >&2
+  exit 1
+fi
+
+if ! which bioawk >/dev/null 2>/dev/null; then
+  echo "Error: cannot find bioawk on PATH" >&2
+  exit 1
+fi
+
+if expr index $region ':' >/dev/null; then
+  colon=$(expr index $region ':')
+  chrom=${region:0:$((colon-1))}
+  region=${region:colon}
+  restrict='$name == "'$chrom'" '
+else
+  restrict=''
+fi
+
+if expr index $region '-' >/dev/null; then
+  dash=$(expr index $region '-')
+  start=${region:0:$((dash-1))}
+  end=${region:dash}
+  length=$((end-start+1))
+else
+  start=$region
+  length=1
+fi
+
+script=$restrict'{print substr($seq, '$start', '$length')}'
+if [[ $# -gt 1 ]]; then
+  for file in ${@:2}; do
+    bioawk -c fastx "$script" $file
+  done
+else
+  bioawk -c fastx "$script"
+fi
