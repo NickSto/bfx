f8bd901a409d4bbee1051ab734277dbc9e89ee99
Nick <nmapsy@gmail.com>
2014-05-12 14:13:16 -0400

    getseq.sh: reorganize usage-printing, argument passing

--- getseq.sh
+++ getseq.sh
@@ -2,22 +2,24 @@
 # original author: Nick Stoler
 set -ue
 
-if [[ $# -ge 1 ]]; then
-  region=$1
-else
-  command=$(basename $0)
-  echo "USAGE: $command region seq.fa [seq2.fa [seq3.fa [...]]]
-       cat seq.fa | $command region
-  e.g. $command chrM:1020-1080 hg19.fa
+command=$(basename $0)
+USAGE="Usage: \$ $command region seq.fa [seq2.fa [seq3.fa [...]]]
+       \$ cat seq.fa | $command region
+  e.g. \$ $command chrM:1020-1080 hg19.fa
 Use bioawk to print the sequence at the given coordinates in a FASTA (or FASTQ)
 file. Use UCSC format for \"region\". Chromosome and ending coordinate are
 optional. If no ending coordinate, it prints a single base at the start
 coordinate. If no chromosome, it prints for every chromosome in the file.
 WARNING:
 bioawk seems to load each chromosome entirely into memory. For hg19.fa, it uses
-about 1GB." >&2
+about 1GB."
+
+if [[ $# -lt 1 ]] || [[ $1 == '-h' ]]; then
+  command=$(basename $0)
+  echo "$USAGE" >&2
   exit 1
 fi
+region=$1
 
 if ! which bioawk >/dev/null 2>/dev/null; then
   echo "Error: cannot find bioawk on PATH" >&2
