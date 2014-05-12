a914a96971fbf71350768eade4c04449e3d2c19f
Nick <nmapsy@gmail.com>
2014-05-12 16:55:06 -0400

    explain-flag.py: Switch to literal bit manipulation using samflags module

--- explain-flag.py
+++ explain-flag.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import argparse
+import samflags
 
 USAGE = "$ %(prog)s [options] flagint [flagint [flagint [..]]]"
 DESCRIPTION = """Decompose a SAM flag integer and print the individual flags
@@ -10,21 +11,7 @@ that are set."""
 EPILOG = """Meanings taken from http://picard.sourceforge.net/explain-flags.html
 but double-checked against the SAM spec."""
 
-MEANINGS = {
-  1:'read paired',
-  2:'read mapped in proper pair',
-  4:'read unmapped',
-  8:'mate unmapped',
-  16:'read reverse strand',
-  32:'mate reverse strand',
-  64:'first in pair',
-  128:'second in pair',
-  256:'not primary alignment',
-  512:'read fails platform/vendor quality checks',
-  1024:'read is PCR or optical duplicate',
-  2048:'supplementary alignment',
-}
-NUM_FLAGS = len(MEANINGS)
+MAX_VALUE = 2**len(samflags.FLAGS) - 1
 
 def main():
 
@@ -45,34 +32,24 @@ def main():
     try:
       flagint = int(flag)
     except ValueError:
-      fail('Error: flag must be an integer. Failed on "'+flag+'"')
-    if flagint > 2**NUM_FLAGS - 1:
-      fail('Error: invalid flag (greater than '+str(2**NUM_FLAGS - 1)
-        +'. Failed on "'+flag+'"')
+      fail('Error: flag must be an integer. Failed on "%s".' % flag)
+    if flagint > MAX_VALUE:
+      fail('Error: invalid flag (greater than %d). Failed on "%s".' % (
+        MAX_VALUE, flag))
 
-    print_flags(get_set_flags(flagint), print_unset=not(args.only_set))
-
-
-def get_set_flags(flagint):
-  flagbin = ('{0:0'+str(NUM_FLAGS)+'b}').format(flagint)
-  i = 0
-  set_flags = [False] * NUM_FLAGS
-  for bit in reversed(flagbin):
-    set_flags[i] = bit == '1'
-    i+=1
-  return set_flags
+    print_flags(samflags.decompose(flagint), print_unset=not(args.only_set))
 
 
-def print_flags(set_flags, print_unset=False):
-  for i in range(len(set_flags)):
+def print_flags(flags_set, print_unset=False):
+  for (flag, meaning) in samflags.FLAGS.items():
     if print_unset:
-      if set_flags[i]:
-        print "[X] %4d %s" % (2**i, MEANINGS[2**i].upper())
+      if flags_set[flag]:
+        print "[X] %4d %s" % (flag, meaning.upper())
       else:
-        print "[ ] %4d %s" % (2**i, MEANINGS[2**i].lower())
+        print "[ ] %4d %s" % (flag, meaning.lower())
     else:
-      if set_flags[i]:
-        print "%4d %s" % (2**i, MEANINGS[2**i])
+      if flags_set[flag]:
+        print "%4d %s" % (flag, meaning)
 
 
 def fail(message):
