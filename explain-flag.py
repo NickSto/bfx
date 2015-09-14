df1a4b05eaa44e43f90e96cc3b2f5d387fa9c67a
me <nmapsy@gmail.com>
2015-09-14 14:36:07 -0400

    explain-flag.py: Switch to 100 character lines.

--- explain-flag.py
+++ explain-flag.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 # original author: Nick Stoler
-import os
 import sys
 import argparse
 import samflags
 
 USAGE = "$ %(prog)s [options] flagint [flagint [flagint [..]]]"
-DESCRIPTION = """Decompose a SAM flag integer and print the individual flags
-that are set."""
-EPILOG = """Meanings taken from http://picard.sourceforge.net/explain-flags.html
-but double-checked against the SAM spec."""
+DESCRIPTION = """Decompose a SAM flag integer and print the individual flags that are set."""
+EPILOG = """Meanings taken from http://picard.sourceforge.net/explain-flags.html but double-checked
+against the SAM spec."""
 
 MAX_VALUE = 2**len(samflags.FLAGS) - 1
 
 def main():
 
-  parser = argparse.ArgumentParser(
-    usage=USAGE, description=DESCRIPTION, epilog=EPILOG
-  )
+  parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION, epilog=EPILOG)
   parser.add_argument('flags', metavar='flagint', nargs='+',
-    help='The integer form of the flags for the read (e.g. "83" for 1, 2, 16, '
-      'and 64).')
+    help='The integer form of the flags for the read (e.g. "83" for 1, 2, 16, and 64).')
   parser.add_argument('-s', '--only-set', action='store_true',
     help='Only print the set flags.')
 
@@ -34,8 +29,7 @@ def main():
     except ValueError:
       fail('Error: flag must be an integer. Failed on "%s".' % flag)
     if flagint > MAX_VALUE:
-      fail('Error: invalid flag (greater than %d). Failed on "%s".' % (
-        MAX_VALUE, flag))
+      fail('Error: invalid flag (greater than %d). Failed on "%s".' % (MAX_VALUE, flag))
 
     print_flags(samflags.decompose(flagint), print_unset=not(args.only_set))
 
