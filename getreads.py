814e21e80e5b6e37b0d041fd2ae22c835c85d1ba
Nick <nmapsy@gmail.com>
2018-03-26 17:38:19 -0700

    getreads.py: Allow passing either an open file or a path.

--- getreads.py
+++ getreads.py
@@ -1,3 +1,4 @@
+import os
 """A simple parser for FASTA, FASTQ, SAM, etc. Create generators that just return the read name and
 sequence.
 All format parsers follow this API:
@@ -13,17 +14,28 @@ The properties of Read are:
 """
 
 
-def getparser(filehandle, filetype='fasta'):
+def getparser(input, filetype='fasta'):
+  # Detect whether the input is an open file or a path.
+  # Return the appropriate reader.
   if filetype == 'fasta':
-    return FastaReader(filehandle)
+    return FastaReader(input)
   elif filetype == 'fastq':
-    return FastqReader(filehandle)
+    return FastqReader(input)
   elif filetype == 'sam':
-    return SamReader(filehandle)
+    return SamReader(input)
   elif filetype == 'tsv':
-    return TsvReader(filehandle)
+    return TsvReader(input)
   else:
-    raise ValueError('Illegal argument: filetype="{}"'.format(filetype))
+    raise ValueError('Unrecognized format: {!r}'.format(filetype))
+
+
+def detect_input_type(obj):
+  """Is this an open filehandle, or is it a file path (string)?"""
+  try:
+    os.path.isfile(obj)
+    return 'path'
+  except TypeError:
+    return 'file'
 
 
 class FormatError(Exception):
@@ -42,8 +54,11 @@ class Read(object):
 
 class Reader(object):
   """Base class for all other parsers."""
-  def __init__(self, filehandle):
-    self.filehandle = filehandle
+  def __init__(self, input):
+    self.input = input
+    self.input_type = detect_input_type(input)
+    if self.input_type not in ('path', 'file'):
+      raise ValueError('Input object {!r} not file-like or string-like.'.format(input))
   def __iter__(self):
     return self.parser()
   def bases(self):
@@ -58,18 +73,26 @@ class TsvReader(Reader):
   Column 2: sequence
   Column 3: quality scores (optional)"""
   def parser(self):
-    for line in self.filehandle:
-      fields = line.rstrip('\r\n').split('\t')
-      if len(fields) < 2:
-        continue
-      read = Read()
-      read.name = fields[0]
-      if read.name:
-        read.id = read.name.split()[0]
-      read.seq = fields[1]
-      if len(fields) >= 3:
-        read.qual = fields[2]
-      yield read
+    if self.input_type == 'path':
+      filehandle = open(self.input)
+    elif self.input_type == 'file':
+      filehandle = self.input
+    try:
+      for line in filehandle:
+        fields = line.rstrip('\r\n').split('\t')
+        if len(fields) < 2:
+          continue
+        read = Read()
+        read.name = fields[0]
+        if read.name:
+          read.id = read.name.split()[0]
+        read.seq = fields[1]
+        if len(fields) >= 3:
+          read.qual = fields[2]
+        yield read
+    finally:
+      if self.input_type == 'path':
+        filehandle.close()
 
 
 class SamReader(Reader):
@@ -79,82 +102,106 @@ class SamReader(Reader):
   All alignment lines have 11 or more fields. Other lines will be skipped.
   """
   def parser(self):
-    for line in self.filehandle:
-      fields = line.split('\t')
-      if len(fields) < 11:
-        continue
-      # Skip headers.
-      if fields[0].startswith('@') and len(fields[0]) == 3:
-        continue
-      read = Read()
-      read.name = fields[0]
-      if read.name:
-        read.id = read.name.split()[0]
-      read.seq = fields[9]
-      read.qual = fields[10].rstrip('\r\n')
-      yield read
+    if self.input_type == 'path':
+      filehandle = open(self.input)
+    elif self.input_type == 'file':
+      filehandle = self.input
+    try:
+      for line in filehandle:
+        fields = line.split('\t')
+        if len(fields) < 11:
+          continue
+        # Skip headers.
+        if fields[0].startswith('@') and len(fields[0]) == 3:
+          continue
+        read = Read()
+        read.name = fields[0]
+        if read.name:
+          read.id = read.name.split()[0]
+        read.seq = fields[9]
+        read.qual = fields[10].rstrip('\r\n')
+        yield read
+    finally:
+      if self.input_type == 'path':
+        filehandle.close()
 
 
 class FastaReader(Reader):
   """A simple FASTA parser that reads one sequence at a time into memory."""
   def parser(self):
-    read = Read()
-    while True:
-      line_raw = self.filehandle.readline()
-      if not line_raw:
-        if read.seq:
-          yield read
-        return
-      line = line_raw.strip()
-      # Allow empty lines.
-      if not line:
-        continue
-      if line.startswith('>'):
-        if read.seq:
-          yield read
-        read = Read()
-        read.name = line[1:]  # remove ">"
-        if read.name:
-          read.id = read.name.split()[0]
-        continue
-      else:
-        read.seq += line
+    if self.input_type == 'path':
+      filehandle = open(self.input)
+    elif self.input_type == 'file':
+      filehandle = self.input
+    try:
+      read = Read()
+      while True:
+        line_raw = filehandle.readline()
+        if not line_raw:
+          if read.seq:
+            yield read
+          return
+        line = line_raw.strip()
+        # Allow empty lines.
+        if not line:
+          continue
+        if line.startswith('>'):
+          if read.seq:
+            yield read
+          read = Read()
+          read.name = line[1:]  # remove ">"
+          if read.name:
+            read.id = read.name.split()[0]
+          continue
+        else:
+          read.seq += line
+    finally:
+      if self.input_type == 'path':
+        filehandle.close()
 
 
 class FastqReader(Reader):
   """A simple FASTQ parser. Can handle multi-line sequences, though."""
   def parser(self):
-    read = Read()
-    state = 'header'
-    while True:
-      line_raw = self.filehandle.readline()
-      if not line_raw:
-        if read.seq:
-          yield read
-        return
-      line = line_raw.strip()
-      # Allow empty lines.
-      if not line:
-        continue
-      if state == 'header':
-        if not line.startswith('@'):
-          raise FormatError('line state = "header" but line does not start with "@"')
-        if read.seq:
-          yield read
-        read = Read()
-        read.name = line[1:]  # remove '@'
-        if read.name:
-          read.id = read.name.split()[0]
-        state = 'sequence'
-      elif state == 'sequence':
-        if line.startswith('+'):
-          state = 'plus'
-        else:
-          read.seq += line
-      elif state == 'plus' or state == 'quality':
-        state = 'quality'
-        togo = len(read.seq) - len(read.qual)
-        read.qual += line[:togo]
-        # The end of the quality lines is when we have a quality string as long as the sequence.
-        if len(read.qual) >= len(read.seq):
-          state = 'header'
+    if self.input_type == 'path':
+      filehandle = open(self.input)
+    elif self.input_type == 'file':
+      filehandle = self.input
+    try:
+      read = Read()
+      state = 'header'
+      while True:
+        line_raw = filehandle.readline()
+        if not line_raw:
+          if read.seq:
+            yield read
+          return
+        line = line_raw.strip()
+        # Allow empty lines.
+        if not line:
+          continue
+        if state == 'header':
+          if not line.startswith('@'):
+            raise FormatError('line state = "header" but line does not start with "@"')
+          if read.seq:
+            yield read
+          read = Read()
+          read.name = line[1:]  # remove '@'
+          if read.name:
+            read.id = read.name.split()[0]
+          state = 'sequence'
+        elif state == 'sequence':
+          if line.startswith('+'):
+            state = 'plus'
+          else:
+            read.seq += line
+        elif state == 'plus' or state == 'quality':
+          state = 'quality'
+          togo = len(read.seq) - len(read.qual)
+          read.qual += line[:togo]
+          # The end of the quality lines is when we have a quality string as long as the sequence.
+          if len(read.qual) >= len(read.seq):
+            state = 'header'
+    finally:
+      if self.input_type == 'path':
+        filehandle.close()
