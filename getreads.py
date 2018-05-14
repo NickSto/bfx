36d8e0ed48e06fe130263f94d3b80f26d69bb71e
Nick <nmapsy@gmail.com>
2018-05-14 14:55:03 -0400

    getreads.py: Add parsing quality scores, simple "lines" format.

--- getreads.py
+++ getreads.py
@@ -13,18 +13,25 @@ The properties of Read are:
   qual: The quality scores (unless the format is FASTA).
 """
 
+# Available formats.
+FORMATS = ('fasta', 'fastq', 'sam', 'tsv', 'lines')
 
-def getparser(input, filetype='fasta'):
+QUAL_OFFSETS = {'sanger':33, 'solexa':64}
+
+
+def getparser(input, filetype, qual_format='sanger'):
   # Detect whether the input is an open file or a path.
   # Return the appropriate reader.
   if filetype == 'fasta':
     return FastaReader(input)
   elif filetype == 'fastq':
-    return FastqReader(input)
+    return FastqReader(input, qual_format=qual_format)
   elif filetype == 'sam':
     return SamReader(input)
   elif filetype == 'tsv':
     return TsvReader(input)
+  elif filetype == 'lines':
+    return LineReader(input)
   else:
     raise ValueError('Unrecognized format: {!r}'.format(filetype))
 
@@ -45,26 +52,52 @@ class FormatError(Exception):
 
 
 class Read(object):
-  def __init__(self, name='', seq='', id_='', qual=''):
+  def __init__(self, name='', seq='', id_='', qual='', qual_format='sanger'):
     self.name = name
     self.seq = seq
     self.id = id_
     self.qual = qual
+    self.offset = QUAL_OFFSETS[qual_format]
+  @property
+  def scores(self):
+    if self.qual is None:
+      return None
+    scores = []
+    for qual_char in self.qual:
+      scores.append(ord(qual_char) - self.offset)
+    return scores
 
 
 class Reader(object):
   """Base class for all other parsers."""
-  def __init__(self, input):
+  def __init__(self, input, qual_format='sanger'):
     self.input = input
     self.input_type = detect_input_type(input)
     if self.input_type not in ('path', 'file'):
       raise ValueError('Input object {!r} not file-like or string-like.'.format(input))
+    self.qual_format = qual_format
   def __iter__(self):
     return self.parser()
   def bases(self):
     for read in self.parser():
       for base in read.seq:
         yield base
+
+
+class LineReader(Reader):
+  """A parser for the simplest format: Only the sequence, one line per read."""
+  def parser(self):
+    if self.input_type == 'path':
+      filehandle = open(self.input)
+    elif self.input_type == 'file':
+      filehandle = self.input
+    try:
+      for line in filehandle:
+        read = Read(seq=line.rstrip('\r\n'))
+        yield read
+    finally:
+      if self.input_type == 'path':
+        filehandle.close()
 
 
 class TsvReader(Reader):
@@ -82,7 +115,7 @@ class TsvReader(Reader):
         fields = line.rstrip('\r\n').split('\t')
         if len(fields) < 2:
           continue
-        read = Read()
+        read = Read(qual_format=self.qual_format)
         read.name = fields[0]
         if read.name:
           read.id = read.name.split()[0]
@@ -114,7 +147,7 @@ class SamReader(Reader):
         # Skip headers.
         if fields[0].startswith('@') and len(fields[0]) == 3:
           continue
-        read = Read()
+        read = Read(qual_format=self.qual_format)
         read.name = fields[0]
         if read.name:
           read.id = read.name.split()[0]
@@ -168,7 +201,7 @@ class FastqReader(Reader):
     elif self.input_type == 'file':
       filehandle = self.input
     try:
-      read = Read()
+      read = Read(qual_format=self.qual_format)
       state = 'header'
       while True:
         line_raw = filehandle.readline()
