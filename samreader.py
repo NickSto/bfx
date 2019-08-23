ccf4c4dde87b14c211f3295db7727217714ee5e9
Nick <nmapsy@gmail.com>
2019-08-22 22:22:58 -0400

    samreader.py: Make object-oriented and a little more fancy.

--- samreader.py
+++ samreader.py
@@ -1,27 +1,62 @@
 # A very simple module to hold code for quick parsing of SAM file fields
 import re
-__version__ = '0.7'
-HEADER_REGEX = r'^@[A-Za-z][A-Za-z]$'
-FIELD_NAMES = ('qname', 'flag', 'rname', 'pos', 'mapq', 'cigar', 'rnext',
-  'pnext', 'tlen', 'seq', 'qual')
 
-class FormatError(Exception):
-  def __init__(self, message=None):
-    if message:
-      Exception.__init__(self, message)
+__version__ = '0.8'
+NULL_STR = '*'
+HEADER_REGEX = r'^@[A-Za-z][A-Za-z]$'
+FIELDS = (
+  {'name':'qname', 'type':str},
+  {'name':'flag', 'type':int},
+  {'name':'rname', 'type':str},
+  {'name':'pos', 'type':int},
+  {'name':'mapq', 'type':int},
+  {'name':'cigar', 'type':str},
+  {'name':'rnext', 'type':str},
+  {'name':'pnext', 'type':int},
+  {'name':'tlen', 'type':int},
+  {'name':'seq', 'type':str},
+  {'name':'qual', 'type':str},
+)
+
+class Alignment(object):
+  def __init__(self, fields=None, **kwargs):
+    if fields is not None:
+      if len(fields) < 11:
+        raise FormatError('Fewer than 11 fields (only saw {})'.format(len(fields)))
+      for i, field_info in enumerate(FIELDS):
+        name = field_info['name']
+        if fields[i] == NULL_STR:
+          setattr(self, name, None)
+        else:
+          try:
+            value = field_info['type'](fields[i])
+          except ValueError:
+            raise FormatError('Invalid value in {} column: {!r}'.format(name.upper(), fields[i]))
+          setattr(self, name, value)
+      self.tags = fields[11:]
+    if kwargs:
+      for field_info in FIELDS:
+        name = field_info['name']
+        if name in kwargs:
+          setattr(self, name, kwargs[name])
+      if 'tags' in kwargs:
+        self.tags = tags
+
 
 def read(filehandle, header=False):
   """A simple SAM parser.
   This returns a generator, so it only reads a line at a time.
   Initialize with an open file (or sys.stdin).
-  Yields a dict for each read, mapping the SAM field names (lowercase) to
-  their values. The tags are under the key 'tags', in a list.
+  Yields an `Alignment` object for each read, where the values in the SAM columns are stored
+  as the attributes of the object. The attribute are named after the field names (lowercase) from
+  the spec.
+  The tags are in the attribute `tags`, in a list.
   If "header" argument is True, it will first yield the header lines, one at
   a time, as simple lists of the tab-delimited field values.
   Usage:
-  for fields in SamReader(open('/home/user/alignment.sam', 'rU')):
-    print "QNAME: "+fields['qname']+", POS: "+fields['pos']
-    print len(fields['tags']), "tags present."
+  for fields in read(open('alignment.sam', 'rU')):
+    print("QNAME: "+fields.qname.+", POS: "+str(fields.pos))
+    print(len(fields.tags), "tags present.")
   """
   for line_raw in filehandle:
     line = line_raw.strip()
@@ -30,16 +65,18 @@ def read(filehandle, header=False):
       continue
     fields = line.split('\t')
     # skip header lines by default
-    if fields[0].startswith('@') and re.search(HEADER_REGEX, fields[0]):
+    if is_header(fields):
       if header:
         yield fields
-      else:
-        continue
-    if len(fields) < 11:
-      raise FormatError('Fewer than 11 fields on line:\n'+line_raw)
-    yield _dictify(fields, FIELD_NAMES)
+      continue
+    yield Alignment(fields)
 
-def _dictify(fields, field_names):
-  data = dict(zip(field_names, fields[:11]))
-  data['tags'] = fields[11:]
-  return data
+
+def is_header(fields):
+  return fields[0].startswith('@') and re.search(HEADER_REGEX, fields[0])
+
+
+class FormatError(Exception):
+  def __init__(self, message=None):
+    if message:
+      Exception.__init__(self, message)
