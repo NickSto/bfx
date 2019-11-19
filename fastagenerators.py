25f0339f349bb10095092947d02d36d8e0653311
Nick <nmapsy@gmail.com>
2019-11-18 21:01:47 -0500

    fastagenerators.py: Overhaul FastaLineGenerator and rename classes. - Rename classes to highlight that their purpose: using different size memory buffers.   - FastaReadGenerator -> FastaSeqBuffered.   - FastaLineGenerator -> FastaLineBuffered.   - FastaBaseGenerator -> FastaBaseBuffered. - Change paradigm of FastaLineBuffered usage.   - Iterate over each sequence, and then over each line within each sequence.

--- fastagenerators.py
+++ fastagenerators.py
@@ -5,17 +5,8 @@ import pathlib
 __version__ = '0.10'
 
 
-def detect_input_type(input):
-  if isinstance(input, pathlib.Path):
-    return 'path'
-  elif isinstance(input, str):
-    return 'str'
-  elif isinstance(input, collections.abc.Generator):
-    return 'generator'
-  elif hasattr(input, 'read') and hasattr(input, 'close'):
-    return 'file'
-  else:
-    return None
+class UsageError(Exception):
+  pass
 
 
 class Reader:
@@ -36,7 +27,7 @@ class Reader:
       return self.input
 
 
-class FastaReadGenerator(object):
+class FastaSeqBuffered(object):
   """Read FASTA files and return one whole sequence at a time."""
 
   def __init__(self, filepath):
@@ -60,14 +51,15 @@ class FastaReadGenerator(object):
       yield read
 
 
-class FastaLineGenerator(Reader):
+class FastaLineBuffered(Reader):
   """A simple FASTA parser that only reads a line at a time into memory.
   Usage:
-  fasta = FastaLineGenerator('/home/user/sequence.fasta')
-  for line in fasta:
-    print "There is a sequence with this FASTA identifier: "+fasta.id
-    print "(Its full name is "+fasta.name+".)"
-    print "It has a line with this sequence: "+line
+  fasta = FastaLineBuffered('/home/user/sequence.fasta')
+  for sequence in fasta:
+    print(f'There is a sequence with this FASTA identifier: {sequence.id}')
+    print(f'Its full name is {sequence.name}.')
+    for line in sequence:
+      print(f'It has a line with this sequence: {line}')
   All strings (the line, id, and name) are stripped, and should not end in a
   newline.
   """
@@ -76,56 +68,121 @@ class FastaLineGenerator(Reader):
     super().__init__(input)
     if self.input_type is 'str' and not os.path.isfile(self.input):
       raise IOError(f'File not found: "{self.input!r}"')
-    self.name = None
-    self.id = None
+    self._sequence = None
+    self._initialized = False
 
   def __iter__(self):
-    return self.lines()
+    self._input_iterator = self.get_input_iterator()
+    # Prime it by reading until the first sequence header.
+    line = None
+    while line is None or not line.startswith('>'):
+      try:
+        line_raw = next(self._input_iterator)
+      except StopIteration:
+        self._cleanup()
+        break
+      line = line_raw.strip()
+    self._last_line = line
+    self._initialized = True
+    return self
 
-  #TODO: Give some signal that we just finished a sequence. Otherwise, we can't validate that there
-  #      aren't sequences with identical names one after another.
-  def lines(self):
-    input_iterator = self.get_input_iterator()
+  def __next__(self):
+    if not self._initialized:
+      raise UsageError('Iterator not initialized!')
+    if self._sequence is not None:
+      if not self._sequence.done:
+        # If the caller requested the next sequence before the last was done generating lines,
+        # finish up the previous one by reading lines until we find the next sequence.
+        self._sequence.fast_forward()
+      next_line = self._sequence._last_line
+    else:
+      next_line = self._last_line
+    if next_line is None:
+      self._cleanup()
+      raise StopIteration
+    seq_id, seq_name = parse_header(next_line)
     try:
-      for line_raw in input_iterator:
-        line = line_raw.strip()
-        if not line:
-          continue  # allow empty lines
-        if line.startswith('>'):
-          self.name = line[1:]  # remove ">"
-          if self.name:
-            self.id = self.name.split()[0]
-          else:
-            self.id = ''
-          continue
-        else:
-          yield line
-    finally:
-      if self.input_type in ('path', 'file', 'str'):
-        input_iterator.close()
+      self._sequence = SequenceLineBuffered(seq_id, seq_name, self._input_iterator)
+    except StopIteration:
+      self._cleanup()
+      raise
+    return self._sequence
+
+  def _cleanup(self):
+    if self.input_type in ('path', 'file', 'str'):
+      self._input_iterator.close()
+
+  def extract(self, start, end, chrom=None):
+    """Extract a subsequence based on a start and end coordinate.
+    If chrom is not supplied, it will default to the first sequence (record)
+    encountered in the FASTA file.
+    See SequenceLineBuffered.extract() for details on the rest."""
+    for sequence in self.sequences():
+      if chrom is not None and sequence.id != chrom:
+        continue
+      return sequence.extract(start, end)
+
+
+class SequenceLineBuffered:
+
+  def __init__(self, seq_id, seq_name, input_iterator):
+    self.name = seq_name
+    self.id = seq_id
+    self.done = False
+    self._input_iterator = input_iterator
+    self._last_line = None
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    if self.done:
+      raise UsageError(f'Tried to read from a finished {type(self).__name__}.')
+    line = self._get_next_line()
+    if line.startswith('>'):
+      self._last_line = line
+      self.done = True
+      raise StopIteration
+    else:
+      return line
+
+  def _get_next_line(self):
+    """Return the next nonempty line or raise StopIteration if there are none."""
+    line = None
+    while not line:
+      try:
+        line_raw = next(self._input_iterator)
+      except StopIteration:
+        self.done = True
+        raise StopIteration
+      line = line_raw.strip()
+    return line
+
+  def fast_forward(self):
+    while True:
+      try:
+        next(self)
+      except StopIteration:
+        break
 
   def bases(self):
     """Generator that yields single bases, while still reading a whole line at
     a time underneath.
     This should be the best of both worlds: it yields a base at a time, but it
     reads a line at a time from the file so it's not slow as molasses."""
-    for line in self.lines():
+    for line in self:
       for base in line:
         yield base
 
-  def extract(self, start, end, chrom=None):
+  def extract(self, start, end):
     """Extract a subsequence based on a start and end coordinate.
-    The start and end are inclusive, 1-based. If chrom is not supplied, it will
-    default to the first chromosome (record) encountered in the FASTA file.
-    If the end coordinate is beyond the end of the chromosome, the returned
-    sequence will be truncated to the end of the chromosome. If the start
-    coordinate is beyond the end of the chromosome, an empty string will be
-    returned."""
+    The start and end are inclusive, 1-based. If the end coordinate is beyond the
+    end of the chromosome, the returned sequence will be truncated to the end of the
+    chromosome. If the start coordinate is beyond the end of the chromosome, an
+    empty string will be returned."""
     outseq = ''
     line_start = 1
     for line in self:
-      if chrom is not None and self.id != chrom:
-        continue
       line_end = line_start + len(line) - 1
       # if we haven't encountered the start yet, keep searching
       if line_end < start:
@@ -142,8 +199,8 @@ class FastaLineGenerator(Reader):
 
 
 #TODO: see 0notes.txt
-class FastaBaseGenerator(object):
-  """For when you absolutely have to read one base at a time. VERY SLOW.
+class FastaBaseBuffered(object):
+  """For when you absolutely have to read one base at a time (e.g. very long lines). VERY SLOW.
   Usage:
   fasta = FastaBaseGenerator('/home/user/sequence.fasta')
   for base in fasta:
@@ -192,3 +249,28 @@ class Read(object):
     self.seq = ''
     self.id = ''
     self.name = ''
+
+
+def detect_input_type(input):
+  if isinstance(input, pathlib.Path):
+    return 'path'
+  elif isinstance(input, str):
+    return 'str'
+  elif isinstance(input, collections.abc.Generator):
+    return 'generator'
+  elif hasattr(input, 'read') and hasattr(input, 'close'):
+    return 'file'
+  else:
+    return None
+
+
+def parse_header(line):
+  seq_id = None
+  seq_name = None
+  if line.startswith('>'):
+    seq_name = line[1:]
+    if seq_name:
+      seq_id = seq_name.split()[0]
+    else:
+      seq_id = None
+  return seq_id, seq_name
