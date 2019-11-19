ea99dae3c617ebfd2b975ca4f8b474aa47f4c830
Nick <nmapsy@gmail.com>
2019-11-18 21:06:23 -0500

    getcontext.py: Overhaul read_ref() to take advantage of new FastaLineBuffered class. - Simplify code - iterate over each chrom, then over each base in each chrom. - Also, rename chr -> chrom in code.

--- getcontext.py
+++ getcontext.py
@@ -5,7 +5,7 @@ import logging
 import pathlib
 import sys
 from typing import Optional, Any, Iterable, Iterator, Sequence, Generator, Dict, List, Tuple
-from fastagenerators import FastaLineGenerator
+from fastagenerators import FastaLineBuffered
 assert sys.version_info.major >= 3, 'Python 3 required'
 
 
@@ -23,11 +23,11 @@ def make_argparser() -> argparse.ArgumentParser:
   options = parser.add_argument_group('Options')
   options.add_argument('-f', '--field', type=int, required=True,
     help='Which column in the sites file has the coordinates of the sites. Numbers are 1-based.')
-  options.add_argument('-c', '--chr-field', type=int, required=True,
+  options.add_argument('-c', '--chrom-field', type=int, required=True,
     help='Which column in the sites file has the id of the chromosome of the site. '
       "Numbers are 1-based. Can omit if there's only one chromosome.")
   #TODO:
-  # options.add_argument('-C', '--chr-id',
+  # options.add_argument('-C', '--chrom-id',
   #   help='Assume all sites are in this reference sequence.')
   options.add_argument('-h', '--help', action='help',
     help='Print this argument help text and exit.')
@@ -49,40 +49,41 @@ def main(argv: List[str]) -> int:
 
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
 
-  sites_by_chr = read_sites(args.sites, args.field, args.chr_field)
+  sites_by_chrom = read_sites(args.sites, args.field, args.chrom_field)
 
-  if len(sites_by_chr) == 0:
+  if len(sites_by_chrom) == 0:
     fail('No sites found in input.')
 
-  # for chr_id, sites in sites_by_chr.items():
+  # for chrom_id, sites in sites_by_chrom.items():
   #   for coord in sites:
-  #     print(chr_id, coord, sep='\t')
+  #     print(chrom_id, coord, sep='\t')
 
-  for data in read_ref(args.ref, sites_by_chr):
+  for data in read_ref(args.ref, sites_by_chrom):
     print(*data, sep='\t')
+    pass
 
   return 0
 
 
 def read_sites(
-    infile: Iterable[str], coord_col: int, chr_col: int=None, sep='\t'
+    infile: Iterable[str], coord_col: int, chrom_col: int=None, sep='\t'
   ) -> Dict[str,List[int]]:
-  sites_by_chr: collections.defaultdict = collections.defaultdict(list)
+  sites_by_chrom: collections.defaultdict = collections.defaultdict(list)
   warned_value = False
   warned_index = False
-  if chr_col is None:
-    chr_id = None
+  if chrom_col is None:
+    chrom_id = None
   for line in infile:
     fields = line.rstrip('\r\n').split(sep)
     try:
       coord_str = fields[coord_col-1]
-      if chr_col is not None:
-        chr_id = fields[chr_col-1]
+      if chrom_col is not None:
+        chrom_id = fields[chrom_col-1]
     except IndexError:
       if not warned_index:
         logging.warning(
           f'Warning: Line(s) with not enough fields in sites file. Looking for columns '
-          f'{coord_col} and {chr_col}. Example invalid line: {line!r}'
+          f'{coord_col} and {chrom_col}. Example invalid line: {line!r}'
         )
         warned_index = True
       continue
@@ -96,45 +97,37 @@ def read_sites(
         )
         warned_value = True
       continue
-    sites_by_chr[chr_id].append(coord)
+    sites_by_chrom[chrom_id].append(coord)
   # Sort the lists of coordinates.
-  for sites in sites_by_chr.values():
+  for sites in sites_by_chrom.values():
     sites.sort()
-  return sites_by_chr
+  return sites_by_chrom
 
 
-def read_ref(ref_path: pathlib.Path, sites_by_chr: Dict[str,List[int]]):
-  fasta = FastaLineGenerator(ref_path)
-  sites: Iterator[int] = iter(())
-  site: Optional[int] = None
-  current_chr = None
-  for line in fasta:
-    if fasta.id != current_chr:
-      # Start of a new chr.
-      coord = 0
-      remaining = len(list(sites))
-      if site is not None:
-        remaining += 1
-      if remaining:
-        logging.warning(f'Warning: {remaining} site(s) not found in sequence {current_chr}')
-      if fasta.id in sites_by_chr:
-        sites = iter(sites_by_chr[fasta.id])
-      else:
-        sites = iter(())
-      try:
-        site = next(sites)
-      except StopIteration:
-        site = None
-      current_chr = fasta.id
-    start_coord = coord
-    for offset, base in enumerate(line, 1):
-      coord = start_coord + offset
+def read_ref(ref_path: pathlib.Path, sites_by_chrom: Dict[str,List[int]]):
+  fasta = FastaLineBuffered(ref_path)
+  sites: Iterator[int]
+  site: Optional[int]
+  for chrom in fasta:
+    if chrom.id in sites_by_chrom and sites_by_chrom[chrom.id]:
+      sites = iter(sites_by_chrom[chrom.id])
+    else:
+      continue
+    site = next(sites)
+    for coord, base in enumerate(chrom.bases(), 1):
       while coord == site:
-        yield fasta.id, coord, base
+        yield chrom.id, coord, base
         try:
           site = next(sites)
         except StopIteration:
           site = None
+      if site is None:
+        break
+    remaining = len(list(sites))
+    if site is not None:
+      remaining += 1
+    if remaining:
+      logging.warning(f'Warning: {remaining} site(s) not found in sequence {chrom.id}')
 
 
 def fail(message: Any) -> None:
