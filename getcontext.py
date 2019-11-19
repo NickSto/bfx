72ea96808528cfe5c82be9c918dbd59084f0e3cc
Nick <nmapsy@gmail.com>
2019-11-18 22:22:33 -0500

    getcontext.py: Implement --chrom-id, move some read_sites() code into subfunctions.

--- getcontext.py
+++ getcontext.py
@@ -21,14 +21,13 @@ def make_argparser() -> argparse.ArgumentParser:
     help='File containing the coordinates of the sites to get sequence context for. '
       'Should be tab-delimited. Default: stdin.')
   options = parser.add_argument_group('Options')
-  options.add_argument('-f', '--field', type=int, required=True,
+  options.add_argument('-f', '--field', type=int, default=1,
     help='Which column in the sites file has the coordinates of the sites. Numbers are 1-based.')
-  options.add_argument('-c', '--chrom-field', type=int, required=True,
+  options.add_argument('-c', '--chrom-field', type=int,
     help='Which column in the sites file has the id of the chromosome of the site. '
       "Numbers are 1-based. Can omit if there's only one chromosome.")
-  #TODO:
-  # options.add_argument('-C', '--chrom-id',
-  #   help='Assume all sites are in this reference sequence.')
+  options.add_argument('-C', '--chrom-id',
+    help='Assume all sites are in this reference sequence.')
   options.add_argument('-h', '--help', action='help',
     help='Print this argument help text and exit.')
   logs = parser.add_argument_group('Logging')
@@ -49,7 +48,14 @@ def main(argv: List[str]) -> int:
 
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
 
-  sites_by_chrom = read_sites(args.sites, args.field, args.chrom_field)
+  if args.chrom_field and args.chrom_id:
+    parser.print_help()
+    fail('--chrom-field and --chrom-id are mutually exclusive.')
+  elif not (args.chrom_field or args.chrom_id):
+    parser.print_help()
+    fail('Must provide a --chrom-field or a --chrom-id.')
+
+  sites_by_chrom = read_sites(args.sites, args.field, args.chrom_field, args.chrom_id)
 
   if len(sites_by_chrom) == 0:
     fail('No sites found in input.')
@@ -66,42 +72,64 @@ def main(argv: List[str]) -> int:
 
 
 def read_sites(
-    infile: Iterable[str], coord_col: int, chrom_col: int=None, sep='\t'
+    infile: Iterable[str], coord_col: int, chrom_col: int=None, chrom_id=None, sep='\t'
   ) -> Dict[str,List[int]]:
   sites_by_chrom: collections.defaultdict = collections.defaultdict(list)
+  const_chrom_id = chrom_id
+  chrom_id = None
   warned_value = False
   warned_index = False
-  if chrom_col is None:
-    chrom_id = None
   for line in infile:
     fields = line.rstrip('\r\n').split(sep)
-    try:
-      coord_str = fields[coord_col-1]
-      if chrom_col is not None:
-        chrom_id = fields[chrom_col-1]
-    except IndexError:
-      if not warned_index:
-        logging.warning(
-          f'Warning: Line(s) with not enough fields in sites file. Looking for columns '
-          f'{coord_col} and {chrom_col}. Example invalid line: {line!r}'
-        )
-        warned_index = True
+    chrom_id, coord_str, warned_index = get_field_strs(
+      fields, coord_col, chrom_col, const_chrom_id, line, warned_index
+    )
+    if coord_str is None:
       continue
-    try:
-      coord = int(coord_str)
-    except ValueError:
-      if not warned_value:
-        logging.warning(
-          f'Warning: Line(s) with invalid coordinate in sites file. Example: Found '
-          f'{coord_str!r} in column {coord_col}.'
-        )
-        warned_value = True
+    coord = parse_coord_int(coord_str, coord_col, warned_value)
+    if coord is None:
       continue
     sites_by_chrom[chrom_id].append(coord)
   # Sort the lists of coordinates.
   for sites in sites_by_chrom.values():
     sites.sort()
   return sites_by_chrom
+
+
+def get_field_strs(
+    fields: List[str], coord_col: int, chrom_col: Optional[int], const_chrom_id: Optional[str],
+    line: str, warned_index: bool
+  ):
+  coord_str = chrom_id = None
+  try:
+    coord_str = fields[coord_col-1]
+    if const_chrom_id:
+      chrom_id = const_chrom_id
+    elif chrom_col is not None:
+      chrom_id = fields[chrom_col-1]
+  except IndexError as error:
+    if not warned_index:
+      logging.warning(
+        f'Warning: Line(s) with not enough fields in sites file. Looking for columns '
+        f'{coord_col} and {chrom_col}. Example invalid line: {line!r}'
+      )
+      warned_index = True
+    return None, None, warned_index
+  return chrom_id, coord_str, warned_index
+
+
+def parse_coord_int(coord_str: str, coord_col: int, warned_value: bool):
+  try:
+    coord = int(coord_str)
+  except ValueError:
+    if not warned_value:
+      logging.warning(
+        f'Warning: Line(s) with invalid coordinate in sites file. Example: Found '
+        f'{coord_str!r} in column {coord_col}.'
+      )
+      warned_value = True
+    return None
+  return coord
 
 
 def read_ref(ref_path: pathlib.Path, sites_by_chrom: Dict[str,List[int]]):
