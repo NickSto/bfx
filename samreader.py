3c18923b75860df42f5aacac07f68d5fc3ec6377
Nick <nmapsy@gmail.com>
2020-12-06 14:48:24 -0500

    samreader.py: Add tag parsing, command line validation tool.

--- samreader.py
+++ samreader.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
 # A very simple module to hold code for quick parsing of SAM file fields
+import argparse
+import logging
 import re
+import sys
 try:
   import cigarlib
 except ImportError:
@@ -24,9 +28,11 @@ FIELDS = (
 
 class Alignment(object):
   def __init__(self, fields=None, **kwargs):
+    self._tags = None
+    self._raw_tags = None
     if fields is not None:
       if len(fields) < 11:
-        raise FormatError('Fewer than 11 fields (only saw {})'.format(len(fields)))
+        raise FormatError(f'Fewer than 11 fields (only saw {len(fields)})')
       for i, field_info in enumerate(FIELDS):
         name = field_info['name']
         if fields[i] == NULL_STR:
@@ -35,16 +41,16 @@ class Alignment(object):
           try:
             value = field_info['type'](fields[i])
           except ValueError:
-            raise FormatError('Invalid value in {} column: {!r}'.format(name.upper(), fields[i]))
+            raise FormatError(f'Invalid value in {name.upper()} column: {fields[i]!r}')
           setattr(self, name, value)
-      self.tags = fields[11:]
+      self._raw_tags = fields[11:]
     if kwargs:
       for field_info in FIELDS:
         name = field_info['name']
         if name in kwargs:
           setattr(self, name, kwargs[name])
       if 'tags' in kwargs:
-        self.tags = tags
+        self.tags = kwargs['tags']
     self._length = None
   @property
   def mate(self):
@@ -103,7 +109,7 @@ class Alignment(object):
   @property
   def supplemental(self):
     return self._flag_cmp(2048)
-  # Okay, here's the only non-simple bit:
+  # Read length.
   @property
   def length(self):
     if self._length is None:
@@ -120,6 +126,98 @@ class Alignment(object):
       if op == 'H':
         length += oplen
     return length
+  # Tags.
+  @property
+  def tags(self):
+    # Parse tags lazily.
+    if self._tags is None:
+      self._tags, self._tag_types = self._parse_tags(self._raw_tags)
+    return self._tags
+  @tags.setter
+  def tags(self, value):
+    if not isinstance(value, dict):
+      raise ValueError(f'Tags must be a dict. Received a {type(value)} instead.')
+    self._tags = value
+  @property
+  def tag_types(self):
+    if self._tag_types is None:
+      self._tags, self._tag_types = self._parse_tags(self._raw_tags)
+    return self._tag_types
+  @tag_types.setter
+  def tag_types(self, value):
+    if not isinstance(value, dict):
+      raise ValueError(f'Tag types must be a dict. Received a {type(value)} instead.')
+    self._tag_types = value
+  @classmethod
+  def _parse_tags(cls, tags_list):
+    tags = {}
+    types = {}
+    if tags_list is None:
+      return tags, types
+    for tag_str in tags_list:
+      tag_name, tag_type, value = cls._parse_tag(tag_str)
+      if tag_name in tags:
+        raise FormatError(f'Multiple {tag_name!r} tags seen in one line.')
+      tags[tag_name] = value
+      types[tag_name] = tag_type
+    return tags, types
+  @classmethod
+  def _parse_tag(cls, tag_str):
+    fields = tag_str.split(':')
+    if len(fields) < 3:
+      raise FormatError(f'Invalid tag. Saw {len(fields)} fields instead of 3: {tag_str!r}')
+    tag_name = fields[0]
+    tag_type = fields[1]
+    value_str = ':'.join(fields[2:])
+    if not (
+      len(tag_name) == 2 and (tag_name[0].isupper() or tag_name[0].islower()) and
+      (tag_name[1].isupper() or tag_name[1].islower() or tag_name[1].isnumeric())
+    ):
+      raise FormatError(f'Invalid tag name {tag_name!r} in {tag_str!r}')
+    value = cls._cast_tag(tag_name, tag_type, value_str)
+    return tag_name, tag_type, value
+  @classmethod
+  def _cast_tag(cls, tag_name, tag_type, value_str):
+    if tag_type == 'A':
+      if len(value_str) != 1:
+        raise make_tag_error(tag_name, tag_type, value_str, 'value is not a single character')
+      if value_str in ' \t\r\n':
+        raise make_tag_error(tag_name, tag_type, value_str, 'value is whitespace')
+      value = value_str
+    elif tag_type == 'i':
+      try:
+        value = int(value_str)
+      except ValueError:
+        raise make_tag_error(tag_name, tag_type, value_str, 'value is not an integer')
+    elif tag_type == 'f':
+      try:
+        value = float(value_str)
+      except ValueError:
+        raise make_tag_error(tag_name, tag_type, value_str, 'value is not a float')
+    elif tag_type == 'Z':
+      if len(value_str) < 1:
+        raise make_tag_error(tag_name, tag_type, value_str, 'value is empty')
+      value = value_str
+    elif tag_type == 'H':
+      try:
+        value = bytes.fromhex(value_str)
+      except ValueError:
+        raise make_tag_error(tag_name, tag_type, value_str, 'value is not valid hex')
+    elif tag_type == 'B':
+      if not (len(value_str) >= 1 and value_str[0] in '[cCsSiIf]'):
+        raise make_tag_error(tag_name, tag_type, value_str, 'value does not contain a valid type')
+      #TODO: Properly parse array.
+      value = value_str
+    elif tag_type == 'S':
+      value = value_str
+    else:
+      tag_str = f'{tag_name}:{tag_type}:{value_str}'
+      raise FormatError(f'Invalid tag. Unrecognized type {tag_type!r} in {tag_str!r}')
+    return value
+
+
+def make_tag_error(tag_name, tag_type, value_str, message):
+  return FormatError(f'Invalid {tag_name} tag. Type is {tag_type!r} but {message}: {value_str!r}')
 
 
 def read(filehandle, header=False):
@@ -137,7 +235,7 @@ def read(filehandle, header=False):
     print("QNAME: "+fields.qname.+", POS: "+str(fields.pos))
     print(len(fields.tags), "tags present.")
   """
-  for line_raw in filehandle:
+  for line_num, line_raw in enumerate(filehandle, 1):
     line = line_raw.strip()
     # skip empty lines (whitespace-only)
     if len(line) == 0:
@@ -148,7 +246,11 @@ def read(filehandle, header=False):
       if header:
         yield fields
       continue
-    yield Alignment(fields)
+    try:
+      yield Alignment(fields)
+    except FormatError as error:
+      error.line_num = line_num
+      raise error
 
 
 def is_header(fields):
@@ -156,6 +258,66 @@ def is_header(fields):
 
 
 class FormatError(Exception):
-  def __init__(self, message=None):
+  def __init__(self, message=None, line_num=None):
     if message:
       Exception.__init__(self, message)
+    self.line_num = line_num
+
+
+#################### Command Line Interface ####################
+
+DESCRIPTION = """Tools for working with SAM files."""
+
+
+def make_argparser():
+  parser = argparse.ArgumentParser(add_help=False, description=DESCRIPTION)
+  options = parser.add_argument_group('Options')
+  options.add_argument('command', choices=('validate',),
+    help='The action to take.')
+  options.add_argument('sam', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
+    help='The input SAM file. Omit to read from stdin.')
+  options.add_argument('-h', '--help', action='help',
+    help='Print this argument help text and exit.')
+  logs = parser.add_argument_group('Logging')
+  logs.add_argument('-l', '--log', type=argparse.FileType('w'), default=sys.stderr,
+    help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
+  volume = logs.add_mutually_exclusive_group()
+  volume.add_argument('-q', '--quiet', dest='volume', action='store_const', const=logging.CRITICAL,
+    default=logging.WARNING)
+  volume.add_argument('-v', '--verbose', dest='volume', action='store_const', const=logging.INFO)
+  volume.add_argument('-D', '--debug', dest='volume', action='store_const', const=logging.DEBUG)
+  return parser
+
+
+def main(argv):
+
+  parser = make_argparser()
+  args = parser.parse_args(argv[1:])
+
+  logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
+
+  if args.command == 'validate':
+    validate(args.sam)
+
+
+def validate(sam_file):
+  total = 0
+  for alignment in read(sam_file):
+    len(alignment.tags)
+    total += 1
+  print(f'Saw {total} valid alignments.')
+
+
+def fail(message):
+  logging.critical('Error: '+str(message))
+  if __name__ == '__main__':
+    sys.exit(1)
+  else:
+    raise Exception(message)
+
+
+if __name__ == '__main__':
+  try:
+    sys.exit(main(sys.argv))
+  except BrokenPipeError:
+    pass
