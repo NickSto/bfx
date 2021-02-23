1474e7c44ccd3f86905dd2de052d0bc9c91c4efc
Nick <nmapsy@gmail.com>
2021-02-23 17:36:02 -0500

    samreader.py: Forgot to finish capitalizing the explain_flag() descriptions.

--- samreader.py
+++ samreader.py
@@ -47,16 +47,16 @@ FLAGS = {
 FLAG_DESCRIPTIONS = {
   1: 'Read paired',
   2: 'Read mapped in proper pair',
-  4: 'read unmapped',
-  8: 'mate unmapped',
-  16: 'read reverse strand',
-  32: 'mate reverse strand',
-  64: 'first in pair',
-  128: 'second in pair',
-  256: 'not primary alignment',
-  512: 'read fails platform/vendor quality checks',
-  1024: 'read is pcr or optical duplicate',
-  2048: 'supplementary alignment',
+  4: 'Read unmapped',
+  8: 'Mate unmapped',
+  16: 'Read reverse strand',
+  32: 'Mate reverse strand',
+  64: 'First in pair',
+  128: 'Second in pair',
+  256: 'Not primary alignment',
+  512: 'Read fails platform/vendor quality checks',
+  1024: 'Read is pcr or optical duplicate',
+  2048: 'Supplementary alignment',
 }
 
 class Alignment:
